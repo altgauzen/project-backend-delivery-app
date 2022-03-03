136c948e0906cf4daff97d042a3e42170080bbf7
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2022-03-03 19:06:14 -0300

    :rotating_light: Fix compiler / linter warnings.

--- front-end/src/pages/Products/index.jsx
+++ front-end/src/pages/Products/index.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../../components/Navbar';
 import ProductsService from '../../service/product.service';
 
 function CustomerProducts() {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     new ProductsService()
-      .getProductsAll(localStorage.getItem("token"))
+      .getProductsAll(localStorage.getItem('token'))
       .then((res) => {
         setProducts(res);
       })
