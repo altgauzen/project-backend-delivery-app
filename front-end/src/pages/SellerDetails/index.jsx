2615bb61ebe0ad609182436c21bc266990e7cbb0
Junior030 <juniorhenrique030@gmail.com>
2022-03-15 17:01:21 -0300

    Req contructor

--- front-end/src/pages/SellerDetails/index.jsx
+++ front-end/src/pages/SellerDetails/index.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import OrderDetailsHeader from '../../components/Orders/OrderDetailsHeader';
+import SellerOrdersDetails from '../../components/SellerOrderDetails/SellerOrdersDetails';
 // import OrderProductsTable from '../../components/Orders/OrderProcuctsTable';
 import SellerNavbar from '../../components/Header/SellerNavbar';
 import SalesService from '../../service/sale.service';
 import UserService from '../../service/user.service';
-import OrderProducts from '../../components/Orders/OrderProducts';
+import SellerOrderProducts from '../../components/SellerOrderDetails/SellerOrderProducts';
 
 // import contextValue from '../../context/context';
 // import './ordersDetails.css';
@@ -68,12 +68,12 @@ function SellerDetails() {
         <br />
 
         <h1>Detalhes do Pedido</h1>
-        <OrderDetailsHeader
+        <SellerOrdersDetails
           role={ role }
           orderData={ orderDetails }
           sellerData={ seller }
         />
-        <OrderProducts
+        <SellerOrderProducts
           productsById={ productsById }
         />
         {/* <'OrderProductsTable /> */}
