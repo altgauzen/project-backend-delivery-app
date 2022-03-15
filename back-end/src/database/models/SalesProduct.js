804576119561d905570a8bc625fe0e3f8cd6a5d6
Genesis-Henriques <GenesisHenriques@gmail.com>
2022-03-15 10:11:02 -0300

    .

--- back-end/src/database/models/SalesProduct.js
+++ back-end/src/database/models/SalesProduct.js
@@ -1,30 +1,3 @@
-// module.exports = (sequelize, DataTypes) => {
-//   const sales_product = sequelize.define(
-//     'sales_product', {
-//       saleId: DataTypes.INTEGER,
-//       productId: DataTypes.INTEGER,
-//       quantity: DataTypes.INTEGER },
-//     { 
-//       timestamps: false,
-//       tableName: 'sales_products',
-//       underscored: true
-//     });
-//   sales_product.associate = (models) => {
-//     models.Sale.belongsToMany(models.Product, {
-//       as: 'products',
-//       through: sales_product,
-//       foreignKey: { name: 'saleId', field: 'sale_id' },
-//       otherKey: { name: 'productId', field: 'product_id' },
-//     });
-//     models.Product.belongsToMany(models.Sale, {
-//       as: 'sales',
-//       through: sales_product,
-//       foreignKey: { name: 'productId', field: 'product_id' },
-//       otherKey: { name: 'saleId', field: 'sale_id' }, 
-//     });
-//   };
-//   return sales_product;
-// };
 module.exports = (sequelize, DataTypes) => {
   const SalesProduct = sequelize.define(
     'salesProducts', {
@@ -33,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       quantity: DataTypes.INTEGER },
     {
       timestamps: false,
-      tableName: 'salesproducts',
+      tableName: 'salesProducts',
       underscored: true
     });
   SalesProduct.associate = (models) => {
